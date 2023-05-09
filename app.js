a71c433056a8ca77cfe403d470ccee04f889c979
User <user@Users-MacBook-Pro.local>
2023-05-09 12:13:31 +0300

    routes - ejs

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
+const ejs = require("ejs");
 const app = express();
 
-//middlewares
-app.use(express.static('public'));
+//template engine - conf
+app.set("view engine", "ejs");
 
+//middlewares
+app.use(express.static("public"));
+
+// routes
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-})
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add", (req, res) => {
+  res.render("add");
+});
 
 const port = 3000;
 app.listen(port, () => {
