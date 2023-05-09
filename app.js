20b99db1a38badc1d30ef4fd037f1b129cd5e450
User <user@Users-MacBook-Pro.local>
2023-05-09 10:33:54 +0300

    adjustment app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = () => {
-  console.log("çalıştı");
-};
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  let user = {
+    name: "Hilal",
+    lName: "Öztürk"
+  }
+  res.send(user)
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listen this ${port} port`);
+});
